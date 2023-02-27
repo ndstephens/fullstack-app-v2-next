1367f5e7d7634d179175c5bc24d03465c45467c2
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-02-27 18:19:45 -0500

    add GlassPane component

---
+++ components/GlassPane.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx';
+
+export default function GlassPane({
+  children,
+  className,
+}: React.ComponentPropsWithoutRef<'div'>) {
+  return (
+    <div
+      className={clsx(
+        'rounded-2xl border-2 border-gray-200 backdrop-blur-md backdrop-saturate-200',
+        className
+      )}
+    >
+      {children}!!
+    </div>
+  );
+}
