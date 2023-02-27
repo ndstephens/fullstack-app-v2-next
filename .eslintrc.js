2a73cbebddd86819b91ab80cbeace3888247d66b
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-02-27 14:15:19 -0500

    eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  plugins: ['simple-import-sort'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 'warn',
+    // REACT
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-no-useless-fragment': 'warn',
+    // SIMPLE-IMPORT-SORT
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          // React and Next related packages come first
+          ['^react', '^@react', '^next', '^@next'],
+          // NPM packages
+          ['^@?\\w'],
+          // Type imports
+          ['^@?\\w.*\\u0000$', '^[^.].*\\u0000$', '^\\..*\\u0000$'],
+          // Side effect imports
+          ['^\\u0000'],
+          //* Path aliased
+          ['^@api', '@assets', '@config', '@hooks', '@ui', '@utils'],
+          // Absolute imports
+          ['^(src|public)(/.*|$)'],
+          // Parent imports. Put `..` last
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    // SIMPLE-IMPORT-SORT
+  },
+};
