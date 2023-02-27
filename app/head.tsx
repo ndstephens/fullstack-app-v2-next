bf8677a001cedd56854656ba58aefbab93b8532e
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-02-27 18:19:32 -0500

    create route files

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="./favicon.ico" />
+      <meta name="description" content="Fullstack task app with Next.js 13" />
+      <title>Task App</title>
+    </>
+  );
+}
