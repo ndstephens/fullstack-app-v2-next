579db6b5d0d8abc29fae2bbaae3f78f21d727394
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-02-28 16:10:49 -0500

    add Button component

---
+++ components/Button.tsx
@@ -0,0 +1,59 @@
+import { cva, VariantProps } from 'class-variance-authority';
+
+const buttonClasses = cva(
+  [
+    'rounded-3xl',
+    'font-bold',
+    'hover:scale-110',
+    'active:scale-100',
+    'transition',
+    'duration-200',
+  ],
+  {
+    variants: {
+      intent: {
+        primary: [
+          'bg-violet-500',
+          'text-white',
+          'border-transparent',
+          'hover:bg-violet-600',
+        ],
+        secondary: [
+          'bg-white',
+          'text-black',
+          'border-gray-400',
+          'border-2',
+          'hover:bg-gray-100',
+          // 'hover:border-gray-800',
+        ],
+        text: ['bg-transparent', 'text-black', 'hover:bg-gray-100'],
+      },
+      size: {
+        small: ['text-md', 'px-2', 'py-1'],
+        medium: ['text-lg', 'px-6', 'py-2'],
+        large: ['text-xlg', 'px-8', 'py-4'],
+      },
+    },
+    defaultVariants: {
+      intent: 'primary',
+      size: 'medium',
+    },
+  }
+);
+
+type ButtonProps = React.ComponentPropsWithoutRef<'button'> &
+  VariantProps<typeof buttonClasses>;
+
+export default function Button({
+  children,
+  intent,
+  size,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button className={buttonClasses({ intent, size, className })} {...props}>
+      {children}
+    </button>
+  );
+}
