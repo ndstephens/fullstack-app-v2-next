18ab19a058ef57a2fe6eb32c7aa1c70b291e0887
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-03-01 18:57:54 -0500

    add password hashing and compare functions

---
+++ lib/auth.ts
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = (password: string) => bcrypt.hash(password, 10);
+
+export const comparePasswords = (
+  plainTextPassword: string,
+  hashedPassword: string
+) => bcrypt.compare(plainTextPassword, hashedPassword);
