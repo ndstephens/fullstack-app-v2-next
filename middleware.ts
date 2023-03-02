e9ecaf86d951b1e13e848e066a1d31537b00741e
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-03-02 17:23:39 -0500

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+
+import { jwtVerify } from 'jose';
+
+import type { NextRequest } from 'next/server';
+
+// had to make this again here as the other one is in "auth.ts" with bcrypt which is not supported on edge run-times
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+type JWTPayload = { id: string; email: string };
+const validateJWT = async (jwt: string) => {
+  const { payload } = await jwtVerify(jwt, secret);
+  return payload.payload as JWTPayload;
+};
+
+const PUBLIC_FILE = /\.(.*)$/;
+
+export default async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/static') ||
+    pathname.startsWith('/signin') ||
+    pathname.startsWith('/register') ||
+    PUBLIC_FILE.test(pathname)
+  ) {
+    return NextResponse.next();
+  }
+
+  const jwt = req.cookies.get(process.env.COOKIE_NAME!);
+
+  if (!jwt) {
+    req.nextUrl.pathname = '/signin';
+    return NextResponse.redirect(req.nextUrl);
+  }
+
+  try {
+    await validateJWT(jwt.value);
+    return NextResponse.next();
+  } catch (e) {
+    console.error(e);
+    req.nextUrl.pathname = '/signin';
+    return NextResponse.redirect(req.nextUrl);
+  }
+}
