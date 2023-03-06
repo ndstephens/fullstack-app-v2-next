d372fa9ee734f39a70bc20e914f2d3c5b7ac8141
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-03-06 14:49:19 -0500

    allow bcrypt in server-components

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
     // typedRoutes: true,
-    // serverComponentsExternalPackages: ['bcrypt'],
+    serverComponentsExternalPackages: ['bcrypt'],
   },
 };
 
